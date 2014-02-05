f0cf659b05a9aaafca137a5bbf3264199cd7da90
Ben Trodd <btrodd@gmail.com>
2014-02-04 17:39:05 -0700

    Changed the way shelves are centered and added sections

--- public/js/storeLayout.js
+++ public/js/storeLayout.js
@@ -39,7 +39,7 @@ $(document).ready(function() {
     $('.accordion').accordion("refresh");
     //$('.child-accordion').accordion("refresh");
     shelfIndex++;
-    drawShelves(shelves);
+    drawShelves(shelves, scale);
   });
 
   $('#addSection').click(function () {
@@ -117,11 +117,19 @@ function sections() {
 
 /* --------------- D3 --------------- */
 
-//var w = 
+
 var svg;
-var w = $('#d3').context.activeElement.clientWidth;
+//var w = $('#d3').context.activeElement.clientWidth;
+// look at viewbox example here: http://jsfiddle.net/NKRPe/60/
+var w = 1000;
 var h = w*2/3;
-var strokePadding = 2;
+var strokePadding = 1;
+
+/* Scale needs its domain to size the current of shelves + blank aisles, so update
+the scale domain anytime a shelf is added */
+var scale = d3.scale.linear()
+  .domain([0,2])
+  .rangeRound([0,w]);
 
 $(document).ready( function() {
   svg = d3.select('#d3').append("svg")
@@ -129,23 +137,75 @@ $(document).ready( function() {
     .attr("height", h);
 });
 
-function drawShelves(shelves){
-var rect = svg.selectAll("rect").data(shelves).enter().append("rect")
-    .attr("opacity", 0)
-    .attr("x", function(d,i) {
-      return i*100;
-    })
+function drawShelves(shelves, scale){
+  // Move exisiting shelves and sections
+  svg.transition().selectAll(".shelf")
+      .duration(500)
+      .attr("x", function(d,i) {
+        return scale(i+1) - 25;
+      });
+  svg.transition().selectAll(".section")
+      .duration(500)
+      .attr("x", function(d,i) {
+        return scale(i+1) - 20;
+      });
+
+  // Add the newest shelf
+  svg.selectAll(".shelf").data(shelves).enter().append("rect")
+    .attr("x", w+100) // initialize out of frame, then slide in
     .attr("y", strokePadding)
     .attr("rx", 5)
     .attr("ry", 5)
-    .attr("width", 35)
-    .attr("height", 250)
-    .attr("fill", "#000000")
-    .attr("stroke",  "#66D9EF")
+    .attr("width", 50)
+    .attr("height", 500)
+    .attr("fill", "#E1EFF5")
+    .attr("stroke",  "#2E6E9E")
     .attr("stroke-width", strokePadding)
     .attr("class", "shelf")
     .transition()
+    .duration(500)
+        .attr("x", function(d,i) {
+      return scale(i+1);
+    });
+
+  drawSections(shelves.length - 1, shelves, scale);
+  // update domain of scale... use +1 to offset 0 index, and +1 to add one
+  scale.domain([0,shelves.length + 1 + 1]);
+  // if size of all elements is bigger than w, scale w
+}
+
+function drawSections(shelfIndex, shelves, scale){
+  var sectionScale = d3.scale.linear()
+    .domain([0, shelves[shelfIndex].sections.length + 2])
+    .range([0,500]);
+
+  // Add new sections
+  svg.selectAll(".section").data(shelves[shelfIndex].sections).enter().append("rect")
+    .attr("x", function() {
+      return scale(shelfIndex+1) + 5;
+    })
+    .attr("y", function(d,i) {
+      return sectionScale(i) + 5;
+    })
+    .attr("width", 40) //probably need to change the sizes
+    .attr("height", function() {
+      return 500/shelves[shelfIndex].sections.length - 10;
+    })
+    .attr("rx", 5)
+    .attr("ry", 5)
+    .attr("fill", "#2E6E9E")
+    .attr("class", "section")
+    .attr("opacity", 0)
+    .transition().delay(500)
     .attr("opacity", 1)
     .duration(500);
+  }
+
+/* Starting at 0, every entry 1,4,7,10 (+3) needs to be blank
+make a function that maps a blank aisle if the index requires it
+function drawAisleSpace() {
+  var rect = svg.selectAll("rect").enter().append("rect")
+  .attr(
 }
+*/
 
