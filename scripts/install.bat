bfb331658ad3c85ed2136e0abd0903d73a207366
Jeremy Steward <jeremy@thatgeoguy.ca>
2014-03-05 00:16:58 -0700

    Hopefully figured out the for /f command this time

--- scripts/install.bat
+++ scripts/install.bat
@@ -1,9 +1,6 @@
 ECHO on
-FOR /f %%i IN (
+FOR /f %i IN (
  'dir /ad /b "C:\" ^|findstr /I "mongo"'
-) DO CALL :movedir %%i
-
-:movedir 
-SET "dest=%1"
-ECHO MOVE "C:\"%1 "C:\mongodb\"
+) DO set dest=%i
+ECHO MOVE "C:\"%dest% "C:\mongodb\"
 setx MYPATH "%PATH%;C:\mongodb\bin"
