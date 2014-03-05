fdd55b166a69ad09d7d6f2c3ddd053da8a644536
Jeremy Steward <jeremy@thatgeoguy.ca>
2014-03-05 00:25:31 -0700

    Tweaked for loop some more to add %%i

--- scripts/install.bat
+++ scripts/install.bat
@@ -1,4 +1,4 @@
 ECHO on
-FOR /f %i IN ('dir /ad /b "C:\" ^|findstr /I "mongo"') DO set dest=%i
+FOR /f %%i IN ('dir /ad /b "C:\" ^|findstr /I "mongo"') DO set dest=%%i
 ECHO MOVE "C:\"%dest% "C:\mongodb\"
 setx MYPATH "%PATH%;C:\mongodb\bin"
