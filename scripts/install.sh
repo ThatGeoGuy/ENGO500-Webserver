235ccd53bf1fd33ce1140193b8a255f93c1b68dc
Jeremy Steward <jeremy@thatgeoguy.ca>
2014-03-04 23:17:34 -0700

    Updated install scriptsw

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,8 @@
 TARFILE=$(find ./ -type f | grep "tgz")
 tar zxvf $TARFILE -C scripts/ 
-mkdir scripts/mongodb-linux/
-mv scripts/mongodb-linux-*/* scripts/mongodb-linux/
-rm -r scripts/mongodb-linux-x*
-DIR=$(realpath "$(pwd)/scripts/mongodb-linux/bin/")
-PATH=$PATH:DIR
+mkdir scripts/mongodb/
+mv scripts/mongodb-linux-*/* scripts/mongodb/
+rm -r scripts/mongodb-linux*
+DIR=$(realpath "$(pwd)/scripts/mongodb/bin/")
+PATH=$PATH:$DIR
 export PATH
