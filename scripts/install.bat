9af6cfc26cd1f63d5f0d8cc0d481ec3c6b6004b7
Jeremy Steward <jeremy@thatgeoguy.ca>
2014-03-05 21:20:43 -0700

    Revert "Removed for loop and used MOVE command with wildcards"
    This reverts commit d4e2e10962b2c4e05b9f54cef8d12e6e4cdff7f2.
    
    I am an idiot and glob commands / wildcards don't work in batch
    scripts. Thanks Microsoft.

--- scripts/install.bat
+++ scripts/install.bat
@@ -1,3 +1,4 @@
-ECHO off
-ECHO MOVE C:\mongo* C:\mongodb
+ECHO on
+FOR /f %%i IN ('dir /ad /b "C:\" ^|findstr /I "mongo"') DO set dest=%%i
+ECHO MOVE C:\%dest% C:\mongodb\
 setx MYPATH "%PATH%;C:\mongodb\bin"
